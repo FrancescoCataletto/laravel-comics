2fa170d3125c1b1f955463b72b21e4179cd56664
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-24 00:11:05 +0200

    added header links with cycle and added style

---
+++ config/headerlinks.php
@@ -0,0 +1,47 @@
+<?php
+
+$link = [
+    [
+        'link' => 'CHARACTERS',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'COMICS',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'MOVIES',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'TV',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'GAMES',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'COLLECTIBLES',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'VIDEOS',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'FANS',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'NEWS',
+        'href'=> '#'
+    ],
+    [
+        'link' => 'SHOP',
+        'href'=> '#'
+    ]
+];
+
+return $link;
+?>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,19 @@
 <header>
-    <H1>HEADER</H1>
-    <ul>
-        <li><a href="{{url('/')}}">Home</a></li>
-        <li><a href="{{route('comics')}}">Comics</a></li>
-        <li><a href="{{route('about')}}">About</a></li>
-        <li><a href="{{route('contact')}}">Contact</a></li>
-    </ul>
+    <!-- container principale header -->
+    <div id="header-box" class="container">
+        <!-- div del logo -->
+        <div>
+            <img src="../img/dc-logo.png" alt="Logo piccolo">
+        </div>
+        <!-- div del menu -->
+        <div>
+            <ul>
+            @foreach($link as $link)
+                <li>
+                    <a href="">{{$link['link']}}</a>
+                </li>
+            @endforeach
+            </ul>
+        </div>
+    </div>
 </header>
