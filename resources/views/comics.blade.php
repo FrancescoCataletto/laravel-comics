ce78dca107e762b2b072a9e0753fa3f351010c26
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-23 14:05:58 +0200

    added yield to main.blade

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    
+    <h1>FUMETTI</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts/main');
 });
+
+Route::get('fumetti', function () {
+    return view('comics');
+})->name('comics');
