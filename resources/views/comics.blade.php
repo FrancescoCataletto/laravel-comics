84a4928e2d6de871573c8e72b8af758707da1c46
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-23 20:51:55 +0200

    made a foreach cycle to print comics in page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.main')
 
 @section('content')
-
-@dump($comics)
+@foreach ($comics as $comic)
+    <div>
+        <img src="{{url($comic->image)}}" alt="">
+        <p>{{$comic->title}}</p>
+        <p>{{$comic->type}}</p>
+    </div>
+@endforeach
 
 @endsection
